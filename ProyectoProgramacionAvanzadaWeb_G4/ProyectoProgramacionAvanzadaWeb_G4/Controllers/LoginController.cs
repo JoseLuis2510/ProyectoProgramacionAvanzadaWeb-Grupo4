84f38eca6bead9222265688f910fe6ca1a135fd5
JoseLuis2510 <josejb425@gmail.com>
2025-05-30 10:16:13 -0600

    Plantilla proyecto
    Plantilla proyecto

---
+++ ProyectoProgramacionAvanzadaWeb_G4/ProyectoProgramacionAvanzadaWeb_G4/Controllers/LoginController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoProgramacionAvanzadaWeb_G4.Models;
+
+namespace ProyectoProgramacionAvanzadaWeb_G4.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Autenticacion autenticacion)
+      
+        {
+            //return View();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult RecuperacionContrasena()
+        {
+            return View();
+        }
+
+        // GET: LoginController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LoginController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LoginController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LoginController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProyectoProgramacionAvanzadaWeb_G4/ProyectoProgramacionAvanzadaWeb_G4/Models/Autenticacion.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoProgramacionAvanzadaWeb_G4.Models
+{
+    public class Autenticacion
+    {
+        public string? CorreoElectronico { get; set; }
+        public string? Contrasenna { get; set; }
+    }
+}
