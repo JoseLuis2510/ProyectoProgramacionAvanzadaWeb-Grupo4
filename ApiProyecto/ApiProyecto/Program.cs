300f57344bfc340a9262d1148d56ed1725cfb18f
JoseLuis2510 <josejb425@gmail.com>
2025-06-28 10:40:11 -0600

    Ajuste interfaz + BD

--- ApiProyecto/ApiProyecto/Program.cs
+++ ApiProyecto/ApiProyecto/Program.cs
@@ -3,6 +3,19 @@ using ApiProyecto.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Agregar servicios de sesión
+builder.Services.AddDistributedMemoryCache(); // Almacén en memoria
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Tiempo de expiración
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddControllersWithViews();
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +28,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSession();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
 
 app.UseExceptionHandler("/api/Error/CapturarError");
 
